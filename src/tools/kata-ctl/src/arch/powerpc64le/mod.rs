81ed8d5f88b25060f8a1f14c25f18f180b9bf7d4
Nicholas Young <hi@secretfader.com>
2024-07-31 16:20:02 -0600

    chore(build): fix compilation on powerpc64 little endian for rust 1.80

--- src/tools/kata-ctl/src/arch/powerpc64le/mod.rs
+++ src/tools/kata-ctl/src/arch/powerpc64le/mod.rs
@@ -4,7 +4,7 @@
 //
 
 use crate::types::*;
-#[cfg(target_arch = "powerpc64le")]
+#[cfg(all(target_arch = "powerpc64", target_endian = "little"))]
 pub use arch_specific::*;
 
 mod arch_specific {
@@ -16,7 +16,7 @@ mod arch_specific {
     pub const ARCH_CPU_MODEL_FIELD: &str = "model";
 
     pub fn check() -> Result<()> {
-        unimplemented!("Check not implemented in powerpc64le");
+        unimplemented!("Check not implemented in powerpc64 little endian");
     }
 
     pub fn get_checks() -> Option<&'static [CheckItem<'static>]> {
