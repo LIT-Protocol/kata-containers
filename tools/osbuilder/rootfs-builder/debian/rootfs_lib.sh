18781770287bfb0950a6f33c40f985cc796d9e46
Nicholas Young <hi@secretfader.com>
2024-08-12 15:19:08 -0600

    feat: add debian rootfs target, based on ubuntu

--- tools/osbuilder/rootfs-builder/debian/rootfs_lib.sh
+++ tools/osbuilder/rootfs-builder/debian/rootfs_lib.sh
@@ -4,4 +4,57 @@
 # SPDX-License-Identifier: Apache-2.0
 
 # NOTE: Re-using ubuntu rootfs lib, see 'ubuntu' folder for details.
-source ${script_dir}/ubuntu/$LIB_SH
+
+PKG_MANAGER="apt"
+
+build_dbus()
+{
+    local ROOTFS_DIR=$1
+
+    info "building dbus"
+    
+    # Prepare directory structure
+    mkdir -p $ROOTFS_DIR/etc/systemd/system
+
+    ln -sf /lib/systemd/system/dbus.service $ROOTFS_DIR/etc/systemd/system/dbus.service
+    ln -sf /lib/systemd/system/dbus.socket $ROOTFS_DIR/etc/systemd/system/dbus.socket
+}
+
+setup_rootfs()
+{
+    local ROOTFS_DIR=$1
+
+    info "configuring rootfs"
+}
+
+
+build_rootfs()
+{
+    local ROOTFS_DIR=$1
+
+    [ -z "$ROOTFS_DIR" ] && die "need rootfs"
+
+    check_root
+    mkdir -p "${ROOTFS_DIR}"
+    
+    apt-get update
+    apt-get upgrade -y
+    apt-get install makedev -qy
+
+    rm -rf ${ROOTFS_DIR}/var
+    rm -rf ${ROOTFS_DIR}/etc
+
+    mkdir -p $ROOTFS_DIR/var
+    ln -s /run $ROOTFS_DIR/var
+
+    mkdir -p $ROOTFS_DIR/etc/ssl/certs
+    cp --remove-destination /etc/ssl/certs/ca-certificates.crt $ROOTFS_DIR/etc/ssl/certs
+
+    # Clean up rootfs directory and reduce size by removing unused files
+    rm -rf ${ROOTFS_DIR}/usr/share/{bash-completion,cracklib,doc,info,locale,man,misc,pixmaps,terminfo,zoneinfo,zsh} 
+
+    mkdir -p $ROOTFS_DIR/dev
+    pushd $ROOTFS_DIR/dev
+    MAKEDEV -v console tty ttyS null zero fd
+    popd
+}
